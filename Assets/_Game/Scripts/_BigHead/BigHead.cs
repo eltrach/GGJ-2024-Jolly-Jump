e41c0f965341c27cef440982bf0ecd603cdd5919
Amchart jamal <jamalamchart@gmail.com>
2024-01-27 09:45:39 +0100

    Big head Collider + MAP 1

--- Assets/_Game/Scripts/_BigHead/BigHead.cs
+++ Assets/_Game/Scripts/_BigHead/BigHead.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BigHead : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] SkinnedMeshRenderer _skinnedMeshRenderer;
+    [SerializeField] float _maxHp;
+
+    float _hp;
+
+    private void Start()
     {
-        
+        if(!_skinnedMeshRenderer)
+            _skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+
+        _hp = _maxHp;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Hit(float damage)
     {
-        
+        _hp -= damage;
+        _skinnedMeshRenderer.SetBlendShapeWeight(0, 100 - (_hp / _maxHp) * 100);
     }
 }
